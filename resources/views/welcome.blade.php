4c80c57d9a23c8f65d2a6188176d2a2911b71c1c
Paing Soe Khant <babyboss7916@gmail.com>
2024-06-10 20:11:50 +0630

    [Changed]Nav bar style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,36 @@
 <html lang="en">
 
 <link>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rest Api</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-<body>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Fake API</title>
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    <div class="container text-light">
-        <nav class="navbar navbar-expand-lg navbar-light bg-white">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Rest Api</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  <a class="nav-link" href="#">Docs</a>
-                  <a class="nav-link" href="#">GitHub</a>
+<body class="main">
 
-                </div>
-              </div>
+    <div class="container mt-4">
+        <div class="row text-secondary">
+            <div class="col-6 ">Fake API</div>
+            <div class="col-6  d-flex justify-content-evenly">
+                <a href="" class=" text-decoration-none text-secondary">
+                    <p>Home</p>
+                </a>
+                <a href="" class=" text-decoration-none text-secondary">
+                    <p>Docs</p>
+                </a>
+                <a href="" class=" text-decoration-none text-secondary">
+                    <p>GitHub</p>
+                </a>
+                <a href="" class=" text-decoration-none text-secondary">
+                    <p>Buy me a coffee</p>
+                </a>
+
+
             </div>
-          </nav>
+        </div>
     </div>
 
 </body>
+
 </html>
