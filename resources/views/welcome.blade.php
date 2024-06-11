5ad2960ef938f9666701594e249528cfb8157c3d
Paing Soe Khant <babyboss7916@gmail.com>
2024-06-11 19:59:02 +0630

    [Add]Main Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Fake API</title>
-@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+{{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <body class="main">
 
@@ -14,7 +16,7 @@
         <div class="row text-secondary">
             <div class="col-6 ">Fake API</div>
             <div class="col-6  d-flex justify-content-evenly">
-                <a href="" class=" text-decoration-none text-secondary">
+                <a href="" class=" text-decoration-none text-secondary :hover">
                     <p>Home</p>
                 </a>
                 <a href="" class=" text-decoration-none text-secondary">
@@ -27,11 +29,35 @@
                     <p>Buy me a coffee</p>
                 </a>
 
-
             </div>
+        </div>
+        <div class="row d-flex justify-center mt-4">
+
+            <div class="col-6 mt-4">
+                <h2 class="font-weight-bold">Fake API</h2>
+                <p>Fake rest API for your e-commerce or shopping website prototype</p>
+              <a href="">
+                <button class="btn btn-primary">View on Github <i class="fa-brands fa-github"></i> </button>
+              </a>
+              <a href="" class="ms-2">
+                <button class="btn btn-success">Read Docs <i class="fa-solid fa-book"></i> </button>
+              </a>
+            </div>
+
+            <div class="col-6 mt-4">
+                image
+            </div>
+
         </div>
     </div>
 
+    <div class="container mt-4">
+
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
